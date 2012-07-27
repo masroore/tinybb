da5cec0f7d8ce40703f3d7dad8bc14907e2c7523
masroore <masroore@gmail.com>
2012-07-27 08:32:18 +0600

    added forum index page - WIP

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,14 +20,14 @@ return array(
 
     'modules' => array(
         // uncomment the following to enable the Gii tool
-        //*
+        /*
         'gii' => array(
             'class' => 'system.gii.GiiModule',
             'password' => '123',
             // If removed, Gii defaults to localhost only. Edit carefully to taste.
             'ipFilters' => array('127.0.0.1', '::1'),
         ),
-        //*/
+        */
     ),
 
     // application components
@@ -55,7 +55,7 @@ return array(
         'urlManager' => array(
             'urlFormat' => 'path',
             'showScriptName' => false,
-            'caseSensitive'=>false,
+            'caseSensitive' => false,
             'rules' => require(dirname(__FILE__) . '/routes.php'),
         ),
         //*
